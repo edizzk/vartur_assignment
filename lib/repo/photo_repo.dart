a8eae713877d0792046f96fbb08cda1fea430f1a
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-27 08:50:44 +0300

    create photo repository

---
+++ lib/repo/photo_repo.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+import '../model/photo_model.dart';
+import '../utils/constants.dart';
+import 'api_status.dart';
+
+class PhotoRepository{
+  static Future<Object> getUsers() async {
+    try {
+      var url = Uri.parse(sampleUrl);
+      var response = await http.get(url);
+      if (response.statusCode == SUCCESS) {
+        return Success(response: photoFromJson(response.body));
+      }
+      return Failure(code: USER_INVALID_RESPONSE, errorResponse: 'Invalid Response');
+    } on HttpException{
+      return Failure(code: NO_INTERNET, errorResponse: 'No Internet Connection');
+    } on FormatException {
+      return Failure(code: INVALID_FORMAT, errorResponse: 'Invalid Format');
+    } catch (e) {
+      return Failure(code: UNKNOWN_ERROR, errorResponse: 'Unknown Error');
+    }
+  }
+}
