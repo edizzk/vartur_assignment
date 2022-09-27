383e1fa584d1e5284113749dfed3f93f6efdc272
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-27 08:52:27 +0300

    create photo detail view

---
+++ lib/view/photo_detail_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PhotoDetailView extends StatelessWidget {
+  const PhotoDetailView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
