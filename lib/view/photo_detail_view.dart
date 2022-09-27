85d376306cd56caf3096f6ae87ec39c1e05f567a
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-27 12:10:42 +0300

    build detail photo card

--- lib/view/photo_detail_view.dart
+++ lib/view/photo_detail_view.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:vartur_assignment/model/photo_model.dart';
 
 class PhotoDetailView extends StatelessWidget {
-  const PhotoDetailView({Key? key}) : super(key: key);
+  const PhotoDetailView({Key? key, required this.photoModel}) : super(key: key);
+
+  final PhotoModel photoModel;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Card(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+              side: const BorderSide(color: Colors.black54, width: 0.5),
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: _cardItems(),
+        )
+      ),
+    );
+  }
+
+  Widget _cardItems() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 50),
+        _imageBox(),
+        const SizedBox(height: 75, width: 320),
+        Padding(
+          padding: const EdgeInsets.only(right: 50, left: 50),
+          child: Text(photoModel.title, textAlign: TextAlign.center, style: const TextStyle(fontSize: 20)),
+        ),
+        const SizedBox(height: 50),
+      ],
+    );
+  }
+
+  Widget _imageBox() {
+    return SizedBox(
+      height: 240,
+      width: 160,
+      //    child: Image.network(photoModel.url, fit: BoxFit.cover),
+      child: Image.asset("lib/assets/600x600.png", fit: BoxFit.cover),
+    );
   }
 }
