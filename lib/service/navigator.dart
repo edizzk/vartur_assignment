0da2b1c4ab141bb7cc8b86068020d5e173d40f6b
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-27 12:38:01 +0300

    create push replacement methods for bottom nav bar changes

--- lib/service/navigator.dart
+++ lib/service/navigator.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:vartur_assignment/model/photo_model.dart';
+import 'package:vartur_assignment/view/my_account_view.dart';
+import 'package:vartur_assignment/view/search_view.dart';
 
+import '../view/home/home_view.dart';
 import '../view/photo_detail_view.dart';
 
 void openDetailsView(BuildContext context, PhotoModel photoModel) {
   Navigator.push(
       context,
-      MaterialPageRoute(
-          builder: (context) => PhotoDetailView(photoModel: photoModel),
-      )
+      MaterialPageRoute(builder: (context) => PhotoDetailView(photoModel: photoModel))
+  );
+}
+void openVarturView(BuildContext context, int selectedIndex) {
+  Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyHomePage(title: "Vartur Assignment", selectedIndex: selectedIndex))
+  );
+}
+
+void openSearchView(BuildContext context, int selectedIndex) {
+  Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => SearchView(selectedIndex: selectedIndex))
+  );
+}
+
+void openMyAccountView(BuildContext context, int selectedIndex) {
+  Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyAccountView(selectedIndex: selectedIndex))
   );
 }
