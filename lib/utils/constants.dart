dd7f82e8d14e76311aee987725d5f07312ad7a85
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-27 08:51:53 +0300

    add constants about api status

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,10 @@
 /// Api Url
 const String sampleUrl = "https://jsonplaceholder.typicode.com/photos";
+
+/// Errors
+const USER_INVALID_RESPONSE = 100;
+const NO_INTERNET = 101;
+const INVALID_FORMAT = 102;
+const UNKNOWN_ERROR = 103;
+
+const SUCCESS = 200;
